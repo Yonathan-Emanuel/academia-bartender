29af8ad7dca770caa7f4ee08fe57d91af131b9df
Yonathan Ortiz <yonathanemanuel@gmail.com>
2024-07-17 22:23:24 -0300

    prueba

---
+++ main.js
@@ -0,0 +1,86 @@
+
+const btnAbrir = document.querySelector("#btn-vmas");
+const btnCerrar = document.querySelector("#btn-cerrar");
+
+const modal = document.querySelector("#modal");
+
+btnAbrir.addEventListener("click",()=>{
+    modal.showModal();
+})
+
+btnCerrar.addEventListener("click",()=>{
+    modal.close();
+})
+
+
+const btnAbrir2 = document.querySelector("#btn-vmas2");
+const btnCerrar2 = document.querySelector("#btn-cerrar2");
+
+const modal2 = document.querySelector("#modal2");
+
+btnAbrir2.addEventListener("click",()=>{
+    modal2.showModal();
+})
+
+btnCerrar2.addEventListener("click",()=>{
+    modal2.close();
+})
+
+const btnAbrir3 = document.querySelector("#btn-vmas3");
+const btnCerrar3 = document.querySelector("#btn-cerrar3");
+
+const modal3 = document.querySelector("#modal3");
+
+btnAbrir3.addEventListener("click",()=>{
+    modal3.showModal();
+})
+
+btnCerrar3.addEventListener("click",()=>{
+    modal3.close();
+})
+
+const btnAbrir4 = document.querySelector("#btn-vmas4");
+const btnCerrar4 = document.querySelector("#btn-cerrar4");
+
+const modal4 = document.querySelector("#modal4");
+
+btnAbrir4.addEventListener("click",()=>{
+    modal4.showModal();
+})
+
+btnCerrar4.addEventListener("click",()=>{
+    modal4.close();
+})
+
+const btnAbrir5 = document.querySelector("#btn-vmas5");
+const btnCerrar5 = document.querySelector("#btn-cerrar5");
+
+const modal5 = document.querySelector("#modal5");
+
+btnAbrir5.addEventListener("click",()=>{
+    modal5.showModal();
+})
+
+btnCerrar5.addEventListener("click",()=>{
+    modal5.close();
+})
+
+const btnAbrir6 = document.querySelector("#btn-vmas6");
+const btnCerrar6 = document.querySelector("#btn-cerrar6");
+
+const modal6 = document.querySelector("#modal6");
+
+btnAbrir6.addEventListener("click",()=>{
+    modal6.showModal();
+})
+
+btnCerrar6.addEventListener("click",()=>{
+    modal6.close();
+})
+
+
+const form = document.querySelector("#btn-form");
+
+form.addEventListener("click",()=>{
+    alert("Tu Mensaje se ha enviado correctamente, Nos Pondremos en contacto a la brevedad.");
+})
